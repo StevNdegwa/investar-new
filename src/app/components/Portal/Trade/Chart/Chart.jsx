a7a0cddc406936858fc0c02db45d386d68cdda33
Stephen <sndegwa.n@outlook.com>
2020-07-26 09:49:07 +0300

    feature(trade):develop stock visualization chart

--- src/app/components/Portal/Trade/Chart/Chart.jsx
+++ src/app/components/Portal/Trade/Chart/Chart.jsx
@@ -8,6 +8,52 @@ import {Wrapper} from "./styles";
 import TradeContext from  "../TradeContext";
 
 export default function Chart({stocksTimeseries, getStocksTimeseries}){
-  return (<Wrapper>
+  const [loading, setLoading] = React.useState(true);
+  const [dataset, setDataset] = React.useState([]);
+  
+  let tradeContext = React.useContext(TradeContext);
+  
+  let margin = 40;
+  
+  let chart = React.useRef();
+  
+  React.useEffect(()=>{
+    
+    loadData();
+  }, [tradeContext.activeItem.item]);
+  
+  async function loadData(){
+    setLoading(true)
+      
+    try{
+     
+      let data = await getStocksTimeseries("DAILY", tradeContext.activeItem.item);
+      console.log(data)
+      setDataset(data);
+      createChart(data);
+      setLoading(false);
+      
+    }catch(error){
+      
+    }
+  }
+  
+  function createChart(dataset){
+    let width = chart.current.width.baseVal.value,
+        height = chart.current.height.baseVal.value,
+        horzScale = d3.scaleUtc(d3.extent(dataset, (d)=>d.date), [0, width-80]),
+        vertScale =  d3.scaleLinear([d3.min(dataset, (d)=>d.low), d3.max(dataset, (d)=>d.high)], [height-80, 0]),
+        area = d3.line().x((d)=>horzScale(d.date)).y0((d)=>0).y1((d)=>vertScale(d.close))
+        
+        
+        console.log(d3.min(dataset, (d)=>d.low))
+        
+    d3.select(chart.current).select(".chart").append("p").datum(dataset).attr("d", area).attr("stroke", "steelblue")
+    .attr("stroke-width", "1.5");
+  }
+  
+  return (<Wrapper ref={chart}>
+    {loading && <ChartLoader/>}
+    <g className="chart" transform={`translate(${margin}, ${margin})`} stroke="steelblue"></g>
   </Wrapper>)
 }
