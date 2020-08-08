4678471682a739cea8f912751af55b06d17a6849
Stephen <sndegwa.n@outlook.com>
2020-08-08 10:26:16 +0300

    chore(pwa):Set-up service workers

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+let CACHE_NAME = "investar-new-cache";
+
+let urlsToCache = [
+  "/",
+  "/assets/images/main-background.jpg"
+]
+
+self.addEventListener("install",function(event){
+  //open cache
+  //cache files
+  //confirm whether required files are cached or not
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache){
+        return cache.addAll(urlsToCache);
+      })
+    )
+  
+})
